ab5205c9a5799cdcf9ce6e07e3a8ce2cd60112a6
gabriel-elesbao <gabriel_elesbao1@hotmail.com>
2021-08-19 16:11:16 -0300

    exercicio4

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,27 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const app = document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+
 `
+
+const appClass = document.querySelector('.app')
+const link = document.querySelector('[data-js="link"]')
+
+link.addEventListener('click', (event)=>{
+  event.preventDefault()
+  if(appClass.style.visibility === 'visible'){
+    link.innerHTML='Clique aqui para exibir'
+     appClass.style.visibility ='hidden'
+  }else{
+    appClass.style.visibility ='visible'
+
+  }
+
+
+}, false)
+
+
+
+
